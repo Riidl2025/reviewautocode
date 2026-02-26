716e942a1d693a00cf682c5c8c38a9a21b71e01a
chaitanyasathe02 <sathechaitanya02@gmail.com>
2026-02-26 15:02:36 +0530

    changed ppt_worker.py code

--- worker/openai_scorer.py
+++ worker/openai_scorer.py
@@ -9,7 +9,7 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
-client = OpenAI(api_key=os.getenv("OPENAI_API_KEY"))
+client = OpenAI(api_key=os.getenv("OPEN_API_KEY"))
 
 
 # -----------------------------
